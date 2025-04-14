dd4cd94303671962103a55fe3c26f74f643b4d8e
Malaka <ithayg@gmail.com>
2025-04-14 22:24:15 +0300

    AddresingMode, need more testing and refactoring

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -20,6 +20,23 @@ enum RegName {
     A,
 }
 
+#[derive(Debug)]
+enum AddressingMode {
+    Implicit,
+    Accumulator,
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Relative,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    Indirect,
+    IndexedIndirect,
+    IndirectIndexed,
+}
+
 const PC_START_ADDR: u16 = 0x8000;
 const NON_MASKABLE_INTER_HNDLER_ADDR: u16 = 0xFFFA;
 const RESET_LOCATION: u16 = 0xFFFC;
@@ -51,7 +68,8 @@ impl CPU6502 {
         let mut break_status: bool = false;
 
         while !break_status {
-            let op_code = self.get_next_byte_in_program();
+            let op_code = self.memory.mem_read(self.program_counter);
+            self.program_counter += 1;
 
             break_status = self.op_code_instraction(op_code);
         }
@@ -72,20 +90,21 @@ impl CPU6502 {
     }
 
     // load and store ops
-    fn lda(&mut self) {
-        self.load_reg(RegName::A);
+    fn lda(&mut self, mode: &AddressingMode) {
+        self.load_reg(RegName::A, mode);
     }
 
-    fn ldx(&mut self) {
-        self.load_reg(RegName::X);
+    fn ldx(&mut self, mode: &AddressingMode) {
+        self.load_reg(RegName::X, mode);
     }
 
-    fn ldy(&mut self) {
-        self.load_reg(RegName::Y);
+    fn ldy(&mut self, mode: &AddressingMode) {
+        self.load_reg(RegName::Y, mode);
     }
 
-    fn load_reg(&mut self, reg_name: RegName) {
-        let param = self.get_next_byte_in_program();
+    fn load_reg(&mut self, reg_name: RegName, mode: &AddressingMode) {
+        let addr = self.get_operand_addr(mode);
+        let val = self.memory.mem_read(addr);
 
         let reg = match reg_name {
             RegName::X => &mut self.indx_reg_x,
@@ -93,26 +112,32 @@ impl CPU6502 {
             RegName::A => &mut self.accumulator,
         };
 
-        *reg = param;
+        *reg = val;
 
         Self::update_zero_flag(*reg, &mut self.status_reg);
         Self::update_negative_flag(*reg, &mut self.status_reg);
+
+        self.move_pc(mode);
     }
 
     // transfer ops
-    fn tax(&mut self) {
+    fn tax(&mut self, mode: &AddressingMode) {
         self.indx_reg_x = self.accumulator;
 
         Self::update_zero_flag(self.indx_reg_x, &mut self.status_reg);
         Self::update_negative_flag(self.indx_reg_x, &mut self.status_reg);
+
+        self.move_pc(mode);
     }
 
     // Increment ops
-    fn inx(&mut self) {
+    fn inx(&mut self, mode: &AddressingMode) {
         self.indx_reg_x = self.indx_reg_x.wrapping_add(1);
 
         Self::update_zero_flag(self.indx_reg_x, &mut self.status_reg);
         Self::update_negative_flag(self.indx_reg_x, &mut self.status_reg);
+
+        self.move_pc(mode);
     }
 
     fn update_zero_flag(reg: u8, status: &mut StatusReg) {
@@ -131,27 +156,120 @@ impl CPU6502 {
         }
     }
 
-    fn get_next_byte_in_program(&mut self) -> u8 {
-        let byte = self.memory.mem_read(self.program_counter);
-        self.program_counter += 1;
+    fn move_pc(&mut self, mode: &AddressingMode) {
+        use AddressingMode::*;
 
-        byte
+        let bytes_to_move = match mode {
+            Implicit | Accumulator => 0,
+            Immediate | ZeroPage | ZeroPageX | ZeroPageY | IndexedIndirect | IndirectIndexed => 1,
+            Relative | Absolute | AbsoluteX | AbsoluteY | Indirect => 2,
+        };
+
+        self.program_counter += bytes_to_move;
     }
 
     fn op_code_instraction(&mut self, op_code: u8) -> bool {
         let mut is_break = false;
 
         match op_code {
-            0xA9 => self.lda(),
-            0xA2 => self.ldx(),
-            0xA0 => self.ldy(),
-            0xAA => self.tax(),
-            0xE8 => self.inx(),
+            0xA9 => self.lda(&AddressingMode::Immediate),
+            0xA2 => self.ldx(&AddressingMode::Immediate),
+            0xA0 => self.ldy(&AddressingMode::Immediate),
+            0xAA => self.tax(&AddressingMode::Implicit),
+            0xE8 => self.inx(&AddressingMode::Implicit),
             0x00 => is_break = true,
             _ => todo!(),
         };
 
         is_break
+    }
+
+    fn get_operand_addr(&mut self, mode: &AddressingMode) -> u16 {
+        match mode {
+            AddressingMode::Implicit => todo!(),
+            AddressingMode::Accumulator => todo!(),
+            AddressingMode::Immediate => self.immediate_addr(),
+            AddressingMode::ZeroPage => self.zero_page_addr(),
+            AddressingMode::ZeroPageX => self.zero_page_x_addr(),
+            AddressingMode::ZeroPageY => self.zero_page_y_addr(),
+            AddressingMode::Relative => self.relative_addr(),
+            AddressingMode::Absolute => self.absolute_addr(),
+            AddressingMode::AbsoluteX => self.absolute_x_addr(),
+            AddressingMode::AbsoluteY => self.absolute_y_addr(),
+            AddressingMode::Indirect => self.relative_addr(),
+            AddressingMode::IndexedIndirect => self.indexed_indirect_addr(),
+            AddressingMode::IndirectIndexed => self.indirect_indexed_addr(),
+        }
+    }
+
+    fn immediate_addr(&self) -> u16 {
+        self.program_counter as u16
+    }
+
+    fn zero_page_addr(&self) -> u16 {
+        let addr = self.memory.mem_read(self.program_counter);
+        addr as u16
+    }
+
+    fn zero_page_x_addr(&self) -> u16 {
+        let param = self.memory.mem_read(self.program_counter);
+        let addr = self.indx_reg_x.wrapping_add(param);
+        addr as u16
+    }
+
+    fn zero_page_y_addr(&self) -> u16 {
+        let param = self.memory.mem_read(self.program_counter);
+        let addr = self.indx_reg_y.wrapping_add(param);
+        addr as u16
+    }
+
+    fn relative_addr(&self) -> u16 {
+        let offset = self.memory.mem_read(self.program_counter);
+        let addr = self.program_counter as i16 + offset as i16;
+        addr as u16
+    }
+
+    fn absolute_addr(&mut self) -> u16 {
+        self.memory.mem_read_u16(self.program_counter)
+    }
+
+    fn absolute_x_addr(&mut self) -> u16 {
+        let param = self.memory.mem_read_u16(self.program_counter);
+        let addr = self.indx_reg_x as u16 + param;
+        addr
+    }
+
+    fn absolute_y_addr(&mut self) -> u16 {
+        let param = self.memory.mem_read_u16(self.program_counter);
+        let addr = self.indx_reg_y as u16 + param;
+        addr
+    }
+
+    fn indirect_addr(&mut self) -> u16 {
+        self.memory.mem_read_u16(self.program_counter)
+    }
+
+    fn indexed_indirect_addr(&self) -> u16 {
+        let param = self.memory.mem_read(self.program_counter);
+        let peek1 = self
+            .memory
+            .mem_read(self.indx_reg_x.wrapping_add(param) as u16);
+        let peek2 = self
+            .memory
+            .mem_read(self.indx_reg_x.wrapping_add(param).wrapping_add(1) as u16)
+            as u16;
+        let addr = peek1 as u16 + (peek2 << 8);
+
+        addr
+    }
+
+    fn indirect_indexed_addr(&self) -> u16 {
+        let param = self.memory.mem_read(self.program_counter);
+        let peek1 = self.memory.mem_read(param as u16);
+        let peek2 = self.memory.mem_read(param.wrapping_add(1) as u16) as u16;
+        let addr = peek1 as u16 + (peek2 << 8) + self.indx_reg_y as u16;
+
+        addr
     }
 }
 
