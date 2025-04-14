d757875aca645c49db335a3eec28a753cdeca91b
Malaka <ithayg@gmail.com>
2025-04-14 15:04:00 +0300

    memmory module, more op codes, tests and refactoring

--- src/emulator/cpu.rs
+++ src/emulator/cpu.rs
@@ -1,6 +1,8 @@
-use status::*;
-
+mod memory;
 mod status;
+
+use memory::*;
+use status::*;
 
 pub struct CPU6502 {
     status_reg: StatusReg,
@@ -9,7 +11,22 @@ pub struct CPU6502 {
     accumulator: u8,
     indx_reg_x: u8,
     indx_reg_y: u8,
+    memory: Memory,
 }
+
+enum RegName {
+    X,
+    Y,
+    A,
+}
+
+const PC_START_ADDR: u16 = 0x8000;
+const NON_MASKABLE_INTER_HNDLER_ADDR: u16 = 0xFFFA;
+const RESET_LOCATION: u16 = 0xFFFC;
+const BRK_INTR_HANDLER_ADDR: u16 = 0xFFFE;
+const ZERO_PAGE: u16 = 0x0000; // 0x0000 - 0x00FF
+const STACK_ADDR: u16 = 0x0100; // 0x0100 - 0x01FF
+const PAGE_SIZE: usize = 0xFF;
 
 impl CPU6502 {
     pub fn new() -> Self {
@@ -20,39 +37,85 @@ impl CPU6502 {
             accumulator: 0,
             indx_reg_x: 0,
             indx_reg_y: 0,
+            memory: Memory::new(),
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory.mem[PC_START_ADDR as usize..(PC_START_ADDR as usize + program.len())]
+            .copy_from_slice(&program[..]);
+        self.memory.mem_write_u16(RESET_LOCATION, PC_START_ADDR);
+    }
+
+    pub fn run(&mut self) {
         let mut break_status: bool = false;
 
         while !break_status {
-            let op_code = program[self.program_counter as usize];
-            self.program_counter += 1;
+            let op_code = self.get_next_byte_in_program();
 
-            break_status = self.op_code_instraction(op_code, &program);
+            break_status = self.op_code_instraction(op_code);
         }
     }
 
-    // load and store ops
-    fn lda(&mut self, program: &[u8]) {
-        let param = program[self.program_counter as usize];
-        self.program_counter += 1;
-        self.accumulator = param;
+    pub fn reset(&mut self) {
+        self.accumulator = 0;
+        self.indx_reg_x = 0;
+        self.indx_reg_y = 0;
+        self.status_reg = StatusReg::new();
+        self.program_counter = self.memory.mem_read_u16(RESET_LOCATION);
+    }
 
-        Self::update_status_zero_flag(self.accumulator, &mut self.status_reg);
-        Self::update_status_negative_flag(self.accumulator, &mut self.status_reg);
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
+
+    // load and store ops
+    fn lda(&mut self) {
+        self.load_reg(RegName::A);
+    }
+
+    fn ldx(&mut self) {
+        self.load_reg(RegName::X);
+    }
+
+    fn ldy(&mut self) {
+        self.load_reg(RegName::Y);
+    }
+
+    fn load_reg(&mut self, reg_name: RegName) {
+        let param = self.get_next_byte_in_program();
+
+        let reg = match reg_name {
+            RegName::X => &mut self.indx_reg_x,
+            RegName::Y => &mut self.indx_reg_y,
+            RegName::A => &mut self.accumulator,
+        };
+
+        *reg = param;
+
+        Self::update_zero_flag(*reg, &mut self.status_reg);
+        Self::update_negative_flag(*reg, &mut self.status_reg);
     }
 
     // transfer ops
     fn tax(&mut self) {
         self.indx_reg_x = self.accumulator;
 
-        Self::update_status_zero_flag(self.indx_reg_x, &mut self.status_reg);
-        Self::update_status_negative_flag(self.indx_reg_x, &mut self.status_reg);
+        Self::update_zero_flag(self.indx_reg_x, &mut self.status_reg);
+        Self::update_negative_flag(self.indx_reg_x, &mut self.status_reg);
     }
 
-    fn update_status_zero_flag(reg: u8, status: &mut StatusReg) {
+    // Increment ops
+    fn inx(&mut self) {
+        self.indx_reg_x = self.indx_reg_x.wrapping_add(1);
+
+        Self::update_zero_flag(self.indx_reg_x, &mut self.status_reg);
+        Self::update_negative_flag(self.indx_reg_x, &mut self.status_reg);
+    }
+
+    fn update_zero_flag(reg: u8, status: &mut StatusReg) {
         if reg == 0 {
             status.set_flag(ZERO_FLAG);
         } else {
@@ -60,7 +123,7 @@ impl CPU6502 {
         }
     }
 
-    fn update_status_negative_flag(reg: u8, status: &mut StatusReg) {
+    fn update_negative_flag(reg: u8, status: &mut StatusReg) {
         if is_negative(reg) {
             status.set_flag(NEGATIVE_FLAG);
         } else {
@@ -68,12 +131,22 @@ impl CPU6502 {
         }
     }
 
-    fn op_code_instraction(&mut self, op_code: u8, program: &[u8]) -> bool {
+    fn get_next_byte_in_program(&mut self) -> u8 {
+        let byte = self.memory.mem_read(self.program_counter);
+        self.program_counter += 1;
+
+        byte
+    }
+
+    fn op_code_instraction(&mut self, op_code: u8) -> bool {
         let mut is_break = false;
 
         match op_code {
-            0xA9 => self.lda(program),
+            0xA9 => self.lda(),
+            0xA2 => self.ldx(),
+            0xA0 => self.ldy(),
             0xAA => self.tax(),
+            0xE8 => self.inx(),
             0x00 => is_break = true,
             _ => todo!(),
         };
--- src/emulator/cpu/test.rs
+++ src/emulator/cpu/test.rs
@@ -3,7 +3,7 @@ use super::*;
 #[test]
 fn lda_0xa9_immediate_load_data() {
     let mut cpu = CPU6502::new();
-    cpu.interpret(vec![0xa9, 0x05, 0x00]);
+    cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
     assert_eq!(cpu.accumulator, 0x05);
     assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
     assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
@@ -12,23 +12,84 @@ fn lda_0xa9_immediate_load_data() {
 #[test]
 fn lda_0xa9_zero_flag() {
     let mut cpu = CPU6502::new();
-    cpu.interpret(vec![0xa9, 0x00, 0x00]);
+    cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
     assert!((cpu.status_reg.status & ZERO_FLAG) == 0b10);
+    assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
 }
 
 #[test]
 fn lda_0xa9_negative_flag() {
     let mut cpu = CPU6502::new();
-    cpu.interpret(vec![0xa9, 0xff, 0x00]);
+    cpu.load_and_run(vec![0xa9, 0xff, 0x00]);
     assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0b1000_0000);
+    assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
+}
+
+#[test]
+fn ldx_0xa2_immediate_load() {
+    let mut cpu = CPU6502::new();
+    cpu.load_and_run(vec![0xa2, 0x05, 0x00]);
+    assert_eq!(cpu.indx_reg_x, 0x05);
+    assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
+    assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
+}
+
+#[test]
+fn ldy_0xa0_immediate_load() {
+    let mut cpu = CPU6502::new();
+    cpu.load_and_run(vec![0xa0, 0x05, 0x00]);
+    assert_eq!(cpu.indx_reg_y, 0x05);
+    assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
+    assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
 }
 
 #[test]
 fn tax_0xaa_move_a_to_x() {
     let mut cpu = CPU6502::new();
+    cpu.load(vec![0xaa, 0x00]);
+    cpu.reset();
     cpu.accumulator = 10;
-    cpu.interpret(vec![0xaa, 0x00]);
+    cpu.run();
     assert_eq!(cpu.indx_reg_x, 10);
     assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
     assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
+}
+
+#[test]
+fn inx_0e8_increment_x() {
+    let mut cpu = CPU6502::new();
+    cpu.load_and_run(vec![0xe8, 0x00]);
+    assert_eq!(cpu.indx_reg_x, 1);
+    assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
+    assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0)
+}
+
+#[test]
+fn inx_0e8_increment_x_negative_flag() {
+    let mut cpu = CPU6502::new();
+    cpu.load(vec![0xe8, 0x00]);
+    cpu.reset();
+    cpu.indx_reg_x = 127;
+    cpu.run();
+    assert_eq!(cpu.indx_reg_x, 128);
+    assert!((cpu.status_reg.status & ZERO_FLAG) == 0);
+    assert!((cpu.status_reg.status & NEGATIVE_FLAG) == 0b1000_0000)
+}
+
+#[test]
+fn test_5_ops_working_together() {
+    let mut cpu = CPU6502::new();
+    cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+    assert_eq!(cpu.indx_reg_x, 0xc1)
+}
+
+#[test]
+fn test_inx_overflow() {
+    let mut cpu = CPU6502::new();
+    cpu.load(vec![0xe8, 0xe8, 0x00]);
+    cpu.reset();
+    cpu.indx_reg_x = 0xff;
+    cpu.run();
+    assert_eq!(cpu.indx_reg_x, 1)
 }
